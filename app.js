9dd9d9b21eef2ad82bae18a4553ce0f4593ff501
telmogoiko <tgoicoechea002@ikasle.ehu.eus>
2024-11-21 12:47:14 +0100

    Agregar rutas para nuevas páginas y script de inicio para desarrollo

--- app.js
+++ app.js
@@ -12,6 +12,31 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src/main/html', 'index.html'));
 });
 
+app.get('/explanation', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/main/html', 'explanation.html'));
+});
+
+app.get('/form', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/main/html', 'form.html'));
+});
+
+app.get('/convocatorias', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/main/html', 'convocatorias.html'));
+});
+
+app.get('/memorias', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/main/html', 'memorias.html'));
+});
+
+app.get('/aboutUs', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/main/html', 'aboutUs.html'));
+});
+
+app.get('/normativa', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/main/html', 'normativa.html'));
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
